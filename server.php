de119f2c08d60fec2879dc5419d30b7311b08a25
Cristian Gil Piñeros <neuracore.apps.dev@gmail.com>
2026-02-02 21:19:54 -0500

    Restore original Laravel server.php

--- server.php
+++ server.php
@@ -1,8 +1,21 @@
 <?php
-echo "<h1>Laravel Application</h1>";
-echo "<p>Server.php is working!</p>";
-echo "<p>URI: " . $_SERVER['REQUEST_URI'] . "</p>";
-echo "<p>Working Dir: " . __DIR__ . "</p>";
-echo "<p>Files in public: </p><pre>";
-print_r(scandir(__DIR__ . '/public'));
-echo "</pre>";
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
